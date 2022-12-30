75d980ca7caff59b2b80fa17f1675386214b5b95
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2022-12-30 13:47:38 +0400

    Created DeliveryForm

--- src/components/ui/DeliveryForm.tsx
+++ src/components/ui/DeliveryForm.tsx
@@ -1,4 +1,45 @@
 import {useState} from "react";
+import Button from "./Button";
+import Input from "./Input";
+import CheckboxItem from "./CheckboxItem";
+
+const deliveryInputsData= [
+    {
+        'type': 'text',
+        'text': 'Категория товара:',
+        'placeholder': 'Категория товара',
+        'required': true,
+        'id': 'df-input-1',
+    },
+    {
+        'type': 'number',
+        'text': 'Стоимость груза:',
+        'placeholder': 'Введите стоимость',
+        'required': true,
+        'id': 'df-input-2',
+    },
+    {
+        'type': 'number',
+        'text': 'Вес груза (кг):',
+        'placeholder': 'Введите вес',
+        'required': true,
+        'id': 'df-input-3',
+    },
+    {
+        'type': 'number',
+        'text': 'Объем груза (м3):',
+        'placeholder': 'Введите объем',
+        'required': true,
+        'id': 'df-input-4',
+    },
+    {
+        'type': 'number',
+        'text': 'Код ТН ВЭД:',
+        'placeholder': 'Введите код',
+        'required': true,
+        'id': 'df-input-5',
+    },
+]
 
 const DeliveryForm = () => {
     // const [inputValue,setInputValue] = useState('')
@@ -23,72 +64,24 @@ const DeliveryForm = () => {
     return(
         <form onSubmit={submitHandler} className="delivery-form">
             <div className="delivery-form__inner">
-                <div className="input-item">
-                    <label htmlFor="input-11" className="input-item__label">Вывести нужный заголовок:</label>
-                    {/*<input*/}
-                    {/*    type="text"*/}
-                    {/*    placeholder="Категория товара"*/}
-                    {/*    className="input-item__input" id="input-11"*/}
-                    {/*    value={inputValue}*/}
-                    {/*    onChange={e => setInputValue(e.target.value)}*/}
-                    {/*/>*/}
-
-                    <input
-                        type="text"
-                        placeholder="Категория товара"
-                        className="input-item__input" id="input-11"
-                        name='name'
-                        value={value.name}
-                        onChange={onChange}
-                    />
-                    <input
-                        type="text"
-                        placeholder="Категория товара"
-                        className="input-item__input" id="input-11"
-                        name='lastName'
-                        value={value.lastName}
-                        onChange={onChange}
-                    />
-
+                <div className="delivery-form__body">
+                    {deliveryInputsData.map(input => (
+                        <Input type={input.type}
+                               title={input.text}
+                               placeholder={input.placeholder}
+                               id={input.id}
+                               required={input.required}
+                               key={input.id}
+                        />
+                    ))}
+                    <div className="delivery-form__checkbox-wrapper">
+                        <CheckboxItem title="Страховка"/>
+                        <CheckboxItem title="Таможенное оформление"/>
+                    </div>
                 </div>
-
-                <div className="input-item">
-                    <label htmlFor="input-22" className="input-item__label">Вывести нужный заголовок:</label>
-                    <input type="text" placeholder="Категория товара" className="input-item__input" id="input-22"/>
+                <div className="delivery-form__controls">
+                    <Button text="Рассчитать" isIcon={true} />
                 </div>
-
-                <div className="input-item">
-                    <label htmlFor="input-3" className="input-item__label">Вывести нужный заголовок:</label>
-                    <input type="text" placeholder="Категория товара" className="input-item__input" id="input-3"/>
-                </div>
-
-                <div className="input-item">
-                    <label htmlFor="input-4" className="input-item__label">Категория товара:</label>
-                    <input type="text" placeholder="Категория товара" className="input-item__input" id="input-4"/>
-                </div>
-
-                <div className="delovery-form__checkbox-wrapper">
-                    <label className="checkbox-item">
-                        <input type="checkbox" className="checkbox-item__input"/>
-                        <span className="checkbox-item__checkbox"></span>
-                        <span className="checkbox-item__title">Таможенное оформление</span>
-                    </label>
-                    <label className="checkbox-item">
-                        <input type="checkbox" className="checkbox-item__input"/>
-                        <span className="checkbox-item__checkbox"></span>
-                        <span className="checkbox-item__title">Таможенное оформление</span>
-                    </label>
-                </div>
-
-                <div className="input-item">
-                    <label htmlFor="input-5" className="input-item__label">Вывести нужный заголовок:</label>
-                    <input type="text" placeholder="Категория товара" className="input-item__input" id="input-5"/>
-                </div>
-
-                <button className="button" onClick={submitHandler}>
-                    Рассчитать
-                    <span className="button__icon"><img src="/img/icons/arrow-circle-left.svg" alt=""/></span>
-                </button>
             </div>
         </form>
     )
