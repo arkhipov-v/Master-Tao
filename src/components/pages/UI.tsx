de071da1432a453eb6a590f98a9d4baec45ed4c4
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-05 17:53:08 +0400

    Small changes

--- src/components/pages/UI.tsx
+++ src/components/pages/UI.tsx
@@ -1,48 +1,59 @@
-import Button from "../ui/Button";
-import Input from "../ui/Input";
-import CheckboxItem from "../ui/CheckboxItem";
-import Headline from "../ui/Headline";
-import CardDelivery from "../ui/CardDelivery";
-import CardBenefit from "../ui/CardBenefit";
-import CardServices from "../ui/CardServices";
-import CardAbout from "../ui/CardAbout";
-import ContactForm from "../ui/ContactForm";
-import DeliveryForm from "../ui/DeliveryForm";
+import Button from "../ui/Button"
+import Input from "../ui/Input"
+import CheckboxItem from "../ui/CheckboxItem"
+import Headline from "../ui/Headline"
+import CardDelivery from "../ui/CardDelivery"
+import CardBenefit from "../ui/CardBenefit"
+import CardServices from "../ui/CardServices"
+import CardAbout from "../ui/CardAbout"
+import ContactForm from "../ui/ContactForm"
+import DeliveryForm from "../ui/DeliveryForm"
 
 const UI = () => {
-    return (
-        <main className="wrapper">
-            {/* TODO Вывести все кнопки использующиеся в проекте*/}
-            <div>
-                <br/>
-                <br/>
-                <Button
-                    text="Buy something"
-                    onClick={() => console.log('test')}
-                    className='my-test-class'
-                    isIcon={true}
-                    isEmpty={true}
-                />
-                <Button
-                    text="Buy something"
-                    onClick={() => console.log('test')}
-                    className='my-test-class'
-                    isIcon={true}
-                />
-                <br/>
-                <br/>
-            </div>
-            <Input type="text" title="Title for input" id="inp-1" placeholder="Text here"/>
-            <CheckboxItem title="Title for checkbox" checked={true}/>
-            <Headline title="Test Headline" />
-            <CardDelivery title="Авто - обычная" img="card-delivery-1" deliveryDays="9 - 15" weight="от 5 кг" price="2,5" />
-            <CardBenefit title='Test 1' icon='test' id='1' />
-            <CardServices />
-            <CardAbout />
-            <DeliveryForm />
-            <ContactForm />
-        </main>
-    )
+	return (
+		<main className="wrapper">
+			{/* TODO Вывести все кнопки использующиеся в проекте*/}
+			<div>
+				<br />
+				<br />
+				<Button
+					text="Buy something"
+					onClick={() => console.log("test")}
+					className="my-test-class"
+					isIcon={true}
+					isEmpty={true}
+				/>
+				<Button
+					text="Buy something"
+					onClick={() => console.log("test")}
+					className="my-test-class"
+					isIcon={true}
+				/>
+				<br />
+				<br />
+			</div>
+			<Input
+				type="text"
+				title="Title for input"
+				id="inp-1"
+				placeholder="Text here"
+			/>
+			<CheckboxItem title="Title for checkbox" checked={true} />
+			<Headline title="Test Headline" />
+			<CardDelivery
+				title="Авто - обычная"
+				img="card-delivery-1"
+				deliveryDays="9 - 15"
+				weight="от 5 кг"
+				price="2,5"
+			/>
+			<CardBenefit title="Test 1" icon="test" id="1" />
+			<CardServices />
+			{/* <CardAbout /> */}
+			<DeliveryForm />
+			<ContactForm />
+		</main>
+	)
 }
 
-export default UI;
+export default UI
--- src/components/ui/Headline.tsx
+++ src/components/ui/Headline.tsx
@@ -1,31 +1,37 @@
-import classnames from 'classnames'
-import cn from 'classnames'
-import {ReactNode} from "react";
+import cn from "classnames"
+import { ReactNode } from "react"
 
 interface IHeadline {
-    title: ReactNode | string
-    description?: string
-    line?: boolean
-    lineOrange?: boolean
-    lineLeft?: boolean
-    className?: string
+	title: ReactNode | string
+	description?: string
+	line?: boolean
+	lineOrange?: boolean
+	lineLeft?: boolean
+	className?: string
 }
 
-const Headline = ({title, description, line, lineOrange, lineLeft, className}: IHeadline) => {
-    return (
-        <div
-             className={cn(
-                 'headline',
-                 className,
-                 line && 'headline--line',
-                 lineOrange && 'headline--line-orange',
-                 lineLeft && 'headline--left'
-             )}
-        >
-            <h2 className="headline__title h2">{title}</h2>
-            <p className="headline__descr">{description}</p>
-        </div>
-    )
+const Headline = ({
+	title,
+	description,
+	line,
+	lineOrange,
+	lineLeft,
+	className,
+}: IHeadline) => {
+	return (
+		<div
+			className={cn(
+				"headline",
+				className,
+				line && "headline--line",
+				lineOrange && "headline--line-orange",
+				lineLeft && "headline--left"
+			)}
+		>
+			<h2 className="headline__title h2">{title}</h2>
+			<p className="headline__descr">{description}</p>
+		</div>
+	)
 }
 
-export default Headline;
+export default Headline
