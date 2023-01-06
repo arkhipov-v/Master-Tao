012ae02d28b66c95160a99c1ef3c2d5e1962717a
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-06 16:12:36 +0400

    Building CardService

--- src/components/ui/CardServices.tsx
+++ src/components/ui/CardServices.tsx
@@ -1,18 +1,27 @@
-const CardServices = () => {
-    return (
-        <div className="wrapper">
-            <div className="card-services">
-                <div className="card-services__banner">
-                    <img src="/img/card-services-1.jpg" alt="Выкуп товара"/>
-                </div>
-                <div className="card-services__info">
-                    <div className="card-services__title">Выкуп товара</div>
-                    <p className="card-services__descr">Компания - это полный комплекс логистических услуг по доставке грузов из Китая в Россию - сборные грузы и цельные товарные партии, услуги посредника ТаоБао, КАРГО Китай-Россия. </p>
-                    <button className="button button--empty">Подробнее</button>
-                </div>
-            </div>
-        </div>
-    )
+import Button from "./Button"
+
+interface CardServiceProps {
+	image: string
+	title: string
+	text: string
+	isButton?: boolean
 }
 
-export default CardServices;
+const CardServices = ({ image, title, text, isButton }: CardServiceProps) => {
+	return (
+		<div className="wrapper">
+			<div className="card-services">
+				<div className="card-services__banner">
+					<img src={`/img/${image}.jpg`} alt="Выкуп товара" />
+				</div>
+				<div className="card-services__info">
+					<div className="card-services__title">{title}</div>
+					<p className="card-services__descr">{text}</p>
+					{isButton && <Button text="Подробнее" isEmpty={true} />}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default CardServices
