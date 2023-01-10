76685405b141961434e807ecd9884da78edbefa2
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-10 14:56:02 +0400

    Create social component

--- src/components/ui/Social.tsx
+++ src/components/ui/Social.tsx
@@ -8,21 +8,23 @@ interface SocialProps {
 const Social = ({ className, isVk, isIns, isTg }: SocialProps) => {
 	return (
 		<div className={`social ${className}`}>
-			{isIns && (
-				<a href="/" className="social__link">
-					<img src="./img/social/instagram.svg" alt="instagram" />
-				</a>
-			)}
-			{isVk && (
-				<a href="/" className="social__link">
-					<img src="./img/social/vkontakte.svg" alt="vkontakte" />
-				</a>
-			)}
-			{isTg && (
-				<a href="/" className="social__link">
-					<img src="/img/social/telegram.svg" alt="telegram" />
-				</a>
-			)}
+			<div className="social__list">
+				{isIns && (
+					<a href="/" target="_blank" className="social__link">
+						<img src="/img/icons/social/instagram.svg" alt="instagram" />
+					</a>
+				)}
+				{isVk && (
+					<a href="/" target="_blank" className="social__link">
+						<img src="./img/icons/social/vkontakte.svg" alt="vkontakte" />
+					</a>
+				)}
+				{isTg && (
+					<a href="/" target="_blank" className="social__link">
+						<img src="/img/icons/social/telegram.svg" alt="telegram" />
+					</a>
+				)}
+			</div>
 		</div>
 	)
 }
