70c710a659f6d3215a988fe623060ec9476af722
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-01-16 15:05:30 +0400

    Add new favicons

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,48 +1,58 @@
-import {useState} from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import TopMenu from "../ui/TopMenu"
 
 const Header = () => {
+	let [stateMenu, setStateMenu] = useState(false)
 
-    let [stateMenu, setStateMenu] = useState(false)
-    console.log('Header render')
+	const openMenu = () => setStateMenu(true)
+	const closeMenu = () => setStateMenu(false)
+	const toggleMenu = () => setStateMenu((prev) => !prev)
 
-    const openMenu = () => setStateMenu(true)
-    const closeMenu = () => setStateMenu(false)
-    const toggleMenu = () => setStateMenu(prev => !prev)
+	return (
+		<header className="header">
+			<div className="wrapper">
+				<div className="header__inner">
+					<Link to="/">
+						<picture>
+							<source media="(min-width: 480px)" srcSet="/img/logo.svg" />
+							<img src="/img/logo-sm.svg" alt="Cargo logo" />
+						</picture>
+					</Link>
 
-    return (
-        <header className="header">
-            <div className="wrapper">
-                <div className="header__inner">
+					<TopMenu
+						className={"header__top-menu"}
+						show={stateMenu}
+						closeHandler={closeMenu}
+					/>
 
-                    <Link to="/">
-                        <picture>
-                            <source media="(min-width: 480px)" srcSet="/img/logo.svg"/>
-                            <img src="/img/logo-sm.svg" alt="Cargo logo"/>
-                        </picture>
-                    </Link>
+					<button className="header__button button button--sm">
+						Оставить заявку
+					</button>
 
-                    <TopMenu
-                        className={'header__top-menu'}
-                        show={stateMenu}
-                        closeHandler={closeMenu}
-                    />
-
-                    <button className="header__button button button--sm">Оставить заявку</button>
-
-                    <button type="button" className="header__burger-menu burger-menu" onClick={openMenu}>
-                        <svg clipRule="evenodd" fillRule="evenodd" strokeLinejoin="round" strokeMiterlimit="2"
-                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                                d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z"
-                                fillRule="nonzero"/>
-                        </svg>
-                    </button>
-                </div>
-            </div>
-        </header>
-    )
+					<button
+						type="button"
+						className="header__burger-menu burger-menu"
+						onClick={openMenu}
+					>
+						<svg
+							clipRule="evenodd"
+							fillRule="evenodd"
+							strokeLinejoin="round"
+							strokeMiterlimit="2"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								d="m11 16.745c0-.414.336-.75.75-.75h9.5c.414 0 .75.336.75.75s-.336.75-.75.75h-9.5c-.414 0-.75-.336-.75-.75zm-9-5c0-.414.336-.75.75-.75h18.5c.414 0 .75.336.75.75s-.336.75-.75.75h-18.5c-.414 0-.75-.336-.75-.75zm4-5c0-.414.336-.75.75-.75h14.5c.414 0 .75.336.75.75s-.336.75-.75.75h-14.5c-.414 0-.75-.336-.75-.75z"
+								fillRule="nonzero"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
